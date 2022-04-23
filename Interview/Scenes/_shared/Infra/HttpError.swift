b7190b890e266c5a55eab64490e3835b0a89b29e
Wagner Coleta <wagnercoleta@gmail.com>
2022-04-23 14:26:11 -0300

    Adicionado pasta para armazenar fontes compartilhados e novo enumerado de erros HttpError.

---
+++ Interview/Scenes/_shared/Infra/HttpError.swift
@@ -0,0 +1,14 @@
+//
+//  HttpError.swift
+//  Interview
+//
+//  Created by Wagner Coleta on 23/04/22.
+//  Copyright © 2022 PicPay. All rights reserved.
+//
+
+import Foundation
+
+enum HttpError: Error {
+    case InvalidURL
+    case request
+}
